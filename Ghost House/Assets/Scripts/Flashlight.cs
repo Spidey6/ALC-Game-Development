2ef7cea44cc5fc323a804e844c794a2dec56cdf4
Matt <matthew.f543@stu.nebo.edu>
2017-10-25 13:11:22 -0600

    Light on and off

---
+++ Ghost House/Assets/Scripts/Flashlight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flashlight : MonoBehaviour {
+
+    public bool lightOn;
+
+	Light light;
+	// Use this for initialization
+	void Start () {
+		light = GetComponent<Light> ();
+		// Set Light default to ON
+		lightOn = true;
+		light.enabled = true;
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Toggle light on/off when l key is pressed.
+		if (Input.GetKeyUp (KeyCode.L) && lightOn) {
+            lightOn = false;
+			light.enabled = true;
+		}
+		
+		else if (Input.GetKeyUp(KeyCode.L) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
+		}
+	
+	}
+	
+}
