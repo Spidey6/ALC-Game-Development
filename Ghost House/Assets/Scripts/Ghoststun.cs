7fbc4fc2a11a9ee279204c3e7fb772bdf2337694
Matt <matthew.f543@stu.nebo.edu>
2017-11-02 13:07:32 -0600

    GhostStun

---
+++ Ghost House/Assets/Scripts/Ghoststun.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ghoststun : MonoBehaviour {
+
+	//bool lightCheck;
+
+	// Use this for initialization
+
+	void Start () {
+		//lightCheck = GetComponent<Flashlight>().lightOn;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerEnter(Collider other){
+		
+
+		if(other.gameObject.name == "Ghost"){
+			print("Ghost is stunned!");
+		}
+	}
+}
