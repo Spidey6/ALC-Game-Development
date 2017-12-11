3842ae6ca8b0fe4439b91e4314776fefa34c5910
Matt <matthew.f543@stu.nebo.edu>
2017-12-11 12:38:20 -0700

    Smooth Follow Camera

---
+++ Ghost House/Assets/Scripts/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowCam : MonoBehaviour {
+	public Transform target;
+	public Vector3 defaultDistance = new Vector3(0f, 2f, -10f);
+	public float distanceDamp = 10f;
+	public float rotationalDamp = 10f;
+
+	Transform myT;
+
+
+	void Awake()
+	{
+		myT = transform;
+	}
+
+
+
+	void LateUpdate()
+	{
+		Vector3 toPros = target.position + (target.rotation * defaultDistance);
+		Vector3 curPros = Vector3.Lerp(myT.position, toPros, distanceDamp * Time.deltaTime);
+		myT.position = curPros;
+
+		Quaternion toRot = Quaternion.LookRotation(target.position - myT.position, target.up);
+		Quaternion curRot = Quaternion.Slerp(myT.rotation, toRot, rotationalDamp * Time.deltaTime);
+		myT.rotation = curRot;
+	}
+}
