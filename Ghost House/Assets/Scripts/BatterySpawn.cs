b5271b902ec31a83dfda55fa028ad304e754326e
Matt <matthew.f543@stu.nebo.edu>
2018-01-09 13:12:38 -0700

    fixes

--- Ghost House/Assets/Scripts/BatterySpawn.cs
+++ Ghost House/Assets/Scripts/BatterySpawn.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 public class BatterySpawn : MonoBehaviour {
 
-
 	public Rigidbody battery;
 
 	public Transform spawnPoint;
@@ -13,61 +12,51 @@ public class BatterySpawn : MonoBehaviour {
 	public bool batSpawned;
 	private bool spawning = false;
 
-
 	// Use this for initialization
 	void Start () {
-		batSpawned = false;
+			batSpawned = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(batSpawned == false){
-			if(batSpawned == false){
 				if(!spawning){
 					spawning = !spawning;
 					StartCoroutine(SpawnBat(spawnTime, battery));
 					print("Spawn Box Empty");
 				}
-			}
-			else if(batSpawned == true){
-				print("Battery has spawned !")
-			}
 		}
-		
-			
-		
+		else if(batSpawned == true){
+			print("Battery has Spawned!");
+		}
+	}
 	
-
-
-		 OnTriggerEnter(Collider other){
-			if(other.gameObject.tag == "Battery"){
-				print("Battery is in the trigger");
-				batSpawned = true;
-			}
-			else{
-				batSpawned = false;
-			}
-
-		 }
-			
-		
-			
-		
-		IEnumerator SpawnBat(float time, Rigidbody bat){
-			yield return new WaitForSeconds(time);
-			Instantiate(bat, spawnPoint.position, spawnPoint.rotation);
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag == "Battery"){
+			print("Spawner is in the trigger");
 			batSpawned = true;
 		}
 	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.tag == "Battery"){
+			print("Spawner is Empty");
+			batSpawned = false;
+		}
+	}
+
+	IEnumerator SpawnBat(float time, Rigidbody bat){
+		yield return new WaitForSeconds(time);
+		Instantiate(bat, spawnPoint.position, spawnPoint.rotation);
+		batSpawned = true;
+		spawning = !spawning;
+	}
+
+
+	public void BatteryPickup(){
+		print("Spawner is Empty");
+		batSpawned = false;
+	}
+
+
 }
-
-
-
-	
-
-
-
-
-
-
-		
